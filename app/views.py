77a66bafe7c0249b3854c04a1afe30003284fb14
Banee Ishaque K <baneeishaque@gmail.com>
2019-10-15 03:51:50 +0530

    Login

--- app/views.py
+++ app/views.py
@@ -3,13 +3,21 @@ from django.shortcuts import render_to_response
 from django.views.generic import FormView
 
 from app.forms import LoginForm
+from app.models import TLogin
 
+
+# TODO : Add Section logic
 
 class LoginFormView(FormView):
     def get(self, request, *args, **kwargs):
         if 'submit' not in request.GET:
-            return render_to_response('app/login.html', dict(form=LoginForm, template_name='app/login.html'))
+            return render_to_response('app/login.html',
+                                      dict(form=LoginForm, template_name='app/login.html'))
         else:
-            username = request.GET['username']
-            passcode = request.GET['passcode']
-            return HttpResponse(username + ' ' + passcode)
+            query_result = TLogin.objects.filter(username=request.GET['username']).filter(
+                passcode=request.GET['passcode'])
+            if len(query_result) == 0:
+                return render_to_response('app/login.html',
+                                          dict(form=LoginForm, template_name='app/login.html', alert_flag=True))
+            else:
+                return HttpResponse('Success')

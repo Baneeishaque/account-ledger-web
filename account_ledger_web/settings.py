5edd00a3105e7a90e083321dea1fd08ae1ff8a04
Banee Ishaque K <Baneeishaque@gmail.com>
2022-11-04 02:15:11 +0530

    Python 3.11

--- account_ledger_web/settings.py
+++ account_ledger_web/settings.py
@@ -136,3 +136,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
